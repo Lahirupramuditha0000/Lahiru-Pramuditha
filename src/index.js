fffc66f9a507406a8f27815611f4d21e0e0dbab7
Lahiru Pramuditha <130990262+Lahirupramuditha0000@users.noreply.github.com>
2023-06-28 08:58:49 +0000

    tried to solove console error

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const userAgentData = navigator.userAgentData || navigator.webkitGetUserAgentData();
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <App userAgentData={userAgentData} />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
