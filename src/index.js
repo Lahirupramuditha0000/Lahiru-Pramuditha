1bb5bbf3b2d8f93dfbaf6ffd95d65c57e52bfa34
Lahiru Pramuditha <130990262+Lahirupramuditha0000@users.noreply.github.com>
2023-07-04 04:35:14 +0000

    removed browser route

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const userAgentData = navigator.userAgentData || navigator.webkitGetUserAgentData();
-
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App userAgentData={userAgentData} />
-    </BrowserRouter>
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
